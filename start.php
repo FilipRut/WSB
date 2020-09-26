0d4c1a2aaae2e20535aba7d63d861ca897bd7db9
Filipe Ruth <35974369+FilipeRuth@users.noreply.github.com>
2020-09-26 16:16:56 +0200

    test

---
+++ start.php
@@ -0,0 +1,14 @@
+﻿<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zmienne PHP</title>
+</head>
+<body>
+    Pierwszy plik<br>
+    <?php
+        echo "WSB";
+       ?>
+</body>
+</html>
